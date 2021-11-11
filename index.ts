63cac5db5888e98df463be62c41e3b7c2d1d9e0f
Petitevale <vrestrepol@unal.edu.co>
2021-11-11 13:49:55 -0500

    Se hace prueba para obtener información del usuario

--- index.ts
+++ index.ts
@@ -1,22 +1,33 @@
+import { createNoSubstitutionTemplateLiteral } from "typescript";
 import conectarBD from "./db/db";
 import {UserModel} from "./models/user";
 
 const main = async () => {
     await conectarBD();
 
-    await UserModel.create({
-        correo:"dsl@c.com",
-        identificacion: "12342",
-        nombre: "pepito",
-        apellido: "Ramirez"
+    // CREAR UN USUARIO
+//     await UserModel.create({
+//         correo:"dsl@c.com",
+//         identificacion: "12342",
+//         nombre: "pepito",
+//         apellido: "Ramirez"
 
-    })
-    .then((u) => {
-        console.log("usuario creado", u);
-    })
-    .catch((e) => {
-        console.error("Error creando el ususario", e)
-    });
+//     })
+//     .then((u) => {
+//         console.log("usuario creado", u);
+//     })
+//     .catch((e) => {
+//         console.error("Error creando el ususario", e)
+//     });
+
+   //OBTENER LOS USUARIOS
+   await UserModel.find().then((u) => {
+       console.log("usuarios", u);
+   })
+   .catch((e) => {
+       console.error("error obteniendo los usuarios", e)
+   });
+    
 };
 
 main()
