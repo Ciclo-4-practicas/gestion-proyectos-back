9c57f2215dc0d038c666084505de10f3b8300b82
Petitevale <vrestrepol@unal.edu.co>
2021-11-10 22:01:44 -0500

    Se instala Mongoose

--- db/db.js
+++ db/db.js
@@ -1,1 +1,11 @@
 import { connect } from "mongoose";
+
+const conectarBD = async () =>{
+return await connect('mongodb+srv://admin:AdminCodex@gestorcodexproject.7gfui.mongodb.net/GestionProyectos?retryWrites=true&w=majority'
+).then(() =>{
+    console.log('Conexión exitosa');
+}).catch((e)=>{
+    console.error("Error conectando a la bd", e);
+})
+};
+export default conectarBD;
