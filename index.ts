a1a8936b693a5868a8e69a4452019cf435c4aa7f
Petitevale <vrestrepol@unal.edu.co>
2021-11-11 12:29:15 -0500

    se conecta la base de datos y se agrega un usuario para probar que guarda la colección en mongo

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
 import conectarBD from "./db/db";
+import {UserModel} from "./models/user";
 
 const main = async () => {
     await conectarBD();
+
+    await UserModel.create({
+        correo:"dsl@c.com",
+        identificacion: "12342",
+        nombre: "pepito",
+        apellido: "Ramirez"
+
+    })
+    .then((u) => {
+        console.log("usuario creado", u);
+    })
+    .catch((e) => {
+        console.error("Error creando el ususario", e)
+    });
 };
 
 main()
